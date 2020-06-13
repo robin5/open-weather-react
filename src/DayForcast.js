6709ed15668438283b8caba95f72114571ffe9c0
Robin Murray <robin.murray360@gmail.com>
2020-06-12 17:44:27 -0700

    Fixes duplicate IDs for forcast components

--- src/DayForcast.js
+++ src/DayForcast.js
@@ -15,31 +15,31 @@ class DayForcast extends React.Component {
     return (
       <div className="day-wrapper">
         <div className="day-header">
-          <span id="day-1">{this.props.day}</span>
+          <span id={this.props.id + "-day"}>{this.props.day}</span>
         </div>
         <div className="day-report-wrapper">
           <img
-            id="day-1-img"
+            id={this.props.id + "-img"}
             className="day-report-weather-icon"
             alt="weather-icon"
             src={this.getImgUrl(this.props.forcast.weather[0].icon)}
           />
-          <span id="day-1-temp-high" className="day-report-temp">
+          <span id={this.props.id + "-temp-high"} className="day-report-temp">
             {this.toCelsius(this.props.forcast.main.temp_max)}
           </span>
-          <span id="day-1-temp" className="day-report-temp">
+          <span id={this.props.id + "-temp"} className="day-report-temp">
             Now
           </span>
-          <span id="day-1-temp-low" className="day-report-temp">
+          <span id={this.props.id + "-temp-low"} className="day-report-temp">
           {this.toCelsius(this.props.forcast.main.temp_min)}
           </span>
-          <span id="day-1-humidity" className="day-report-measure">
+          <span id={this.props.id + "-humidity"} className="day-report-measure">
             Humidity: {this.props.forcast.main.humidity}
           </span>
-          <span id="day-1-pressure" className="day-report-measure">
+          <span id={this.props.id + "-pressure"} className="day-report-measure">
             Pressure: {this.props.forcast.main.pressure}
           </span>
-          <span id="day-1-wind" className="day-report-measure">
+          <span id={this.props.id + "-wind-speed"} className="day-report-measure">
             Wind: {this.props.forcast.wind.speed} MPH
           </span>
         </div>
